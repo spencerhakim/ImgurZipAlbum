dc040fa2963e73348878187df4dcd31a869cbfb5
Spencer Hakim <spencerhakim@gmail.com>
2012-05-24 15:43:45 -0400

    Try to fix premature removal of status div

--- main.js
+++ main.js
@@ -85,8 +85,9 @@ var ImgurZipAlbum = (function() {
         {
             console.log('Generating zip...');
             statusDiv.html('<img src="'+BASEURL+'media/loader.gif" style="vertical-align:text-bottom" /> Generating zip... (your browser may freeze during this process)');
-            location.href = "data:application/zip;base64," + zip.generate(); //don't use compression, takes up too much CPU
+            var uri = "data:application/zip;base64," + zip.generate(); //don't use compression, takes up too much CPU
             statusDiv.parent().remove();
+            location.href = uri;
         }
     }
     
